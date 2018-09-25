01bf018fc0f20a353046c6e94d59a011751e8e37
Kostia <hks1153@gmail.com>
2018-09-25 22:33:52 +0300

    forms

--- Form/researches_form.php
+++ Form/researches_form.php
@@ -29,14 +29,46 @@ E-mail: $email
 Конец";
 }*/
 
-print_r($_POST) ;
+/* first try
 
-/*mail($myemail, $tema, $message_to_myemail, $from);*/
+mail($myemail, $tema, $message_to_myemail, $from);
 
-/*
-$mysqli = new mysqli ("localhost", "root", "", "aptresearches_form")
+$conn = mysqli_connect("localhost", "root", "", "aptresearches_form");
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "INSERT INTO `users`(`txtFirstName`, `txtLastName`, `email`, `txtTelephoneNumber`, `txtCompany`, `txtNatureOfBusiness`, `Country`, `titleOfResearche`) VALUES ('$txtFirstName', '$txtLastName', '$email', '$txtTelephoneNumber', '$txtCompany', '$txtNatureOfBusiness', '$Country', '$titleOfResearche') ";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+   echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);*/
+
+/* second try
+
+$mysqli = new mysqli ("localhost", "root", "", "aptresearches_form");
 $mysqli->query ("SET NAMES 'utf8'");
-$mysqli->query ();
+$mysqli->query ("INSERT INTO `users`(`txtFirstName`, `txtLastName`, `email`, `txtTelephoneNumber`, `txtCompany`, `txtNatureOfBusiness`, `Country`, `titleOfResearche`) VALUES (`'$txtFirstName'`,`$txtLastName`,`$email`,`$txtTelephoneNumber`,`$txtCompany`,`$txtNatureOfBusiness`,`$Country`,`$titleOfResearche`)");
+
 $mysqli->close();*/
+
+$con=mysqli_connect("localhost", "root", "", "aptresearches_form");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+// Perform queries 
+mysqli_query($con,"SELECT * FROM users");
+mysqli_query($con,"INSERT INTO users (id, txtFirstName, txtLastName, email, txtTelephoneNumber, txtCompany, txtNatureOfBusiness, Country, titleOfResearche) 
+VALUES (NULL, '$txtFirstName', '$txtLastName', '$email', '$txtTelephoneNumber', '$txtCompany', '$txtNatureOfBusiness', '$Country', '$titleOfResearche')");
+
+mysqli_close($con);
 
 ?>
